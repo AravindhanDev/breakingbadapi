e48fe852be3bb2435f15a43d89b3ec2ae047b569
ADMIN <NoobieUs37@protonmail.com>
2021-10-19 12:38:07 +0530

    Updates

---
+++ precache-manifest.20f4adcd347576c298b7a1644c4a51e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba69494fa11888c0e27319285b79bb19",
+    "url": "/breakingbadapi/index.html"
+  },
+  {
+    "revision": "c0e544843cfa412027e8",
+    "url": "/breakingbadapi/static/js/2.97bd378f.chunk.js"
+  },
+  {
+    "revision": "58042bdc4fd6d9ac3bd2",
+    "url": "/breakingbadapi/static/js/main.afe4dd3c.chunk.js"
+  },
+  {
+    "revision": "c631e83fcba18ade21ad",
+    "url": "/breakingbadapi/static/js/runtime-main.de191a20.js"
+  }
+]);
